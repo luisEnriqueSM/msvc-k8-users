36294d4719d7806237a488241684079b5a4213d3
luis_enriquesm96@hotmail.com <luis_enriquesm96@hotmail.com>
2025-05-16 23:04:21 -0600

    Remove url from FeignClient connection.

--- src/main/java/org/k8/springcloud/msvc/users/clients/CourseClientRest.java
+++ src/main/java/org/k8/springcloud/msvc/users/clients/CourseClientRest.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "msvc-k8-courses", url = "localhost:8002")
+@FeignClient(name = "msvc-k8-courses")
 public interface CourseClientRest {
 
     @DeleteMapping("/eliminar-curso-usuario/{userId}")
