31f5e386ce31214ed299597e0d618de52d0c3ee0
luis_enriquesm96@hotmail.com <luis_enriquesm96@hotmail.com>
2025-05-20 00:13:42 -0600

    Add url to call msvc-k8-courses container on CourseClientRest.

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 # msvc-k8-users
 
-## Contenedor Mysql
+## Contenedores
 
 ```bash
+
+# Contenedor MySQL
 docker run -d -p 3306:3306 --network springcloud -v mysql_data_k8:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=sasa1234 -e MYSQL_DATABASE=db_springboot_cloud --name mysql8 mysql:8.0.42
 
 # Crear imagen
--- src/main/java/org/k8/springcloud/msvc/users/clients/CourseClientRest.java
+++ src/main/java/org/k8/springcloud/msvc/users/clients/CourseClientRest.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "msvc-k8-courses")
+@FeignClient(name = "msvc-k8-courses", url = "msvc-k8-courses:8002")
 public interface CourseClientRest {
 
     @DeleteMapping("/eliminar-curso-usuario/{userId}")
